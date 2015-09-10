ecefa9e5911672f8ebfdd69ac778bf8c37237446
smithdamen <smith.damen@gmail.com>
2015-09-10 22:20:09 +0000

    modified scripts

--- src/js/main.js
+++ src/js/main.js
@@ -4,4 +4,16 @@ $(document).ready(function(){
        target: "#navbar-main",
        offset: 50
    });
+   
+   //scrollTo
+   $('a[href^="#"]').click(function(e){
+        e.preventDefault();
+        // console.log(this.hash);
+        $(window).stop(true).scrollTo(this.hash, {duration:550, interrupt:true, easing:'easeInBack'});
+    });
+    
+    //MixItUp
+    $(function(){
+        $('#mix-container').mixItUp();
+    });
 });
