f0c863e23dc4de9f3051c4250b6da00348ea0663
smithdamen <smith.damen@gmail.com>
2015-09-14 23:53:28 +0000

    modified grunt

--- gruntfile.js
+++ gruntfile.js
@@ -1,54 +1,48 @@
 module.exports = function(grunt) {
+    require('load-grunt-tasks')(grunt);
+    // Project configuration.
+    grunt.initConfig({
+        less: {
+            dev: {
+                options: {
+                    paths: ["src/css"]
+                },
+                files: {
+                    "src/css/styles.css": "src/less/main.less"
+                }
+            }
+        },
+        watch: {
+            css: {
+                files: "src/less/*.less",
+                tasks: ['build']
+            }
+        },
+        uglify: {
+            options: {
+                mangle: false
+             },
+             build: {
+                files: {
+                    'src/css/styles.min.css': ['src/css/styles.css']
+                }
+            }
+        },
+        cssmin: {
+            target: {
+                files: [{
+                    expand: true,
+                    cwd: 'src/css',
+                    src: ['*.css', '!*.min.css'],
+                    dest: 'src/css',
+                    ext: '.min.css'
+                }]
+            }
+        }
+    });
 
-  // Project configuration.
-  grunt.initConfig({
-      less: {
-          dev: {
-              options: {
-                  paths: ["src/css"]
-              },
-              files: {
-                  "src/css/styles.css": "src/less/main.less"
-              }
-          }
-      },
-      watch: {
-          css: {
-              files: "src/less/*.less",
-              tasks: ["less"]
-          }
-      },
-      uglify: {
-              options: {
-                    mangle: false
-              },
-              build: {
-                  files: {
-                      'src/css/styles.min.css': ['src/css/styles.css']
-                  }
-              }
-      },
-      cssmin: {
-          target: {
-              files: [{
-                  expand: true,
-                  cwd: 'src/css',
-                  src: ['*.css', '!*.min.css'],
-                  dest: 'src/css',
-                  ext: '.min.css'
-              }]
-          }
-      }
-  });
-
-  // Load the plugin that provides the "uglify" task.
-  grunt.loadNpmTasks('grunt-contrib-uglify');
-  grunt.loadNpmTasks('grunt-contrib-watch');
-  grunt.loadNpmTasks('grunt-contrib-less');
-  grunt.loadNpmTasks('grunt-contrib-cssmin');
-
-  // Default task(s).
-  grunt.registerTask('default', ['less']);
-  grunt.registerTask('default', ['uglify']);
-  grunt.registerTask('default', ['cssmin']);
+    // Default task(s).
+    grunt.registerTask('default', ['watch','build']);
+    grunt.registerTask('build', ['less','cssmin']);
+    
 };
